2b6e9c5f29d2b51253e944c84eacce1b0ca46387
infazse <infaz.10@cse.mrt.ac.lk>
2018-06-05 20:22:58 +0530

    Added UI changes for register and log in

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+  userToken: any;
+
+constructor(private http: Http) {}
+
+login(model: any) {
+  return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).pipe(map((response: Response) => {
+      const user = response.json();
+      if (user) {
+        localStorage.setItem('token', user.tokenString);
+        this.userToken = user.tokenString;
+      }
+  }));
+}
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+}
+
+private requestOptions() {
+  const headers = new Headers({'content-type' : 'application/json'});
+  return new RequestOptions({headers: headers});
+}
+
+}
